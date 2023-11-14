5f0eb1d66fbcd324ede708cd0f69d62599c936ee
alexey_t <alexterleev682004@gmail.com>
2023-11-14 09:31:11 +0300

    small fix

--- app/innotter/apps.py
+++ app/innotter/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InnotterConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "innotter"
+
+    def ready(self):
+        import innotter.signals
--- app/innotter/models.py
+++ app/innotter/models.py
@@ -37,15 +37,3 @@ class Like(models.Model):
     user_id = models.UUIDField(null=False)
 
 
-@receiver(models.signals.post_save, sender=Like)
-def update_post_likes(sender, instance, **kwargs):
-    post = instance.post
-    post.likes = Like.objects.filter(post=post).count()
-    post.save()
-
-
-@receiver(models.signals.post_save, sender=Follower)
-def update_post_likes(sender, instance, **kwargs):
-    page = instance.page
-    page.followers = Follower.objects.filter(page=page).count()
-    page.save()
