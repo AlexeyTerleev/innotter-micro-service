add3c7d69ced40bea0264f7ee4333da1edc7b12e
alexey_t <alexterleev682004@gmail.com>
2023-11-28 11:19:30 +0300

    lint

--- app/app/celery.py
+++ app/app/celery.py
@@ -4,12 +4,12 @@ from celery import Celery
 from celery.schedules import crontab
 
 app = Celery("app")
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'unblock-pages': {
-        'task': 'innotter.tasks.unblock_pages',
-        'schedule': crontab(hour=9, minute=0),
+    "unblock-pages": {
+        "task": "innotter.tasks.unblock_pages",
+        "schedule": crontab(hour=9, minute=0),
     },
 }
--- app/innotter/tasks.py
+++ app/innotter/tasks.py
@@ -1,8 +1,11 @@
 from datetime import date
-from innotter.models import Page
+
 from app.celery import app
+from innotter.models import Page
 
 
 @app.task
-def unblock_pages():    
-    Page.objects.filter(blocked=True, unblock_date__lte=date.today()).update(blocked=False, unblock_date=None)  
+def unblock_pages():
+    Page.objects.filter(blocked=True, unblock_date__lte=date.today()).update(
+        blocked=False, unblock_date=None
+    )
